1a9db5383ea97772b78c2112cc555ba3f98ab9cb
RISTO TAPANI RAIHALA <tyokone@RISTOs-MBP.home>
2018-10-11 17:56:32 +0200

    services + favorites

---
+++ src/services/quotes.ts
@@ -0,0 +1,20 @@
+import { Quote } from "../data/quote.interface";
+
+export class quotesServices {
+    private favoriteQuotes: Quote[] = [];
+
+    addQuoteToFavorites(quote: Quote) {
+        this.favoriteQuotes.push(quote);
+    }
+
+    removeQuoteFromFavorites(quote: Quote){
+        const position = this.favoriteQuotes.findIndex((quoteEl: Quote) => {
+            return quoteEl.id == quote.id;
+        });
+        this.favoriteQuotes.splice(position, 1);
+    }
+
+    getFavoritesQuotes() {
+        return this.favoriteQuotes.slice();
+    }
+}
